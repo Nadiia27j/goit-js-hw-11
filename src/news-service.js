593370cbcd3f5f7e3cc657cd9c21e83a05c1c47e
Надія Ткач <nadiya77723@gmail.com>
2022-09-24 22:10:31 +0300

    ...

--- src/news-service.js
+++ src/news-service.js
@@ -1,86 +1,48 @@
 
 import axios from 'axios';
 
-// export default class NewsApiService {
-//     constructor() {
-//        this.searchQuery = '';
-//        this.page = 1;
-//     }
-    // searchQuery = '';
-    // page = 1;
+export default class NewsApiService {
+    constructor() {
+       this.searchQuery = '';
+       this.page = 1;
+    }
 
+    async  fetchImage() {
+        const options = {
+            method: "GET",
+            image_type: photo,
+            orientation: horizontal,
+            safesearch: true,
 
-   
-       
-        
-
-    // incrementPage() {
-    //     this.page += 1
-    // }
-
-    // resetPage() {
-    //     this.page = 1;
-    // }
-
-    // get query() {
-    //     return this.searchQuery;
-    // }
-
-    // set query (newQuery) {
-    //     this.searchQuery = newQuery;
+        };
     
-
-// }
-
-
-
-
-
-
-
-
-// import axios from 'axios';
-
-// export default class NewsApiService {
-//     constructor() {
-//        this.searchQuery = '';
-//        this.page = 1;
-//     }
-
-//     async  fetchImage() {
-//         const options = {
-//             method: "GET",
-//             image_type: photo,
-//             orientation: horizontal,
-//             safesearch: true,
-//         };
-    
-//         try {
-//             const response = await axios.get(`https://pixabay.com/api/?key='30077711-4b113b89ab0e54a97a0c4d035'&q=${this.searchQuery}&per_page=40&page=${this.page}`, options);
-//             const data = console.log(response);
+        try {
+            const response = await axios.get(`https://pixabay.com/api/?key='30077711-4b113b89ab0e54a97a0c4d035'&q=${this.searchQuery}&per_page=40&page=${this.page}`, options);
+            console.log(response);
+          const data = response.data.hits;
+             return data;
+            this.incrementPage();
           
-//             this.incrementPage();
-//             return data;
 
-//         } catch (error) {
-//             console.error(error);
-//         }   
-//     }
+        } catch (error) {
+            console.error(error);
+        }   
+    }
 
-//     incrementPage() {
-//         this.page += 1
-//     }
+    incrementPage() {
+        this.page += 1
+    }
 
-//     resetPage() {
-//         this.page = 1;
-//     }
+    resetPage() {
+        this.page = 1;
+    }
 
-//     get query() {
-//         return this.searchQuery;
-//     }
+    get query() {
+        return this.searchQuery;
+    }
 
-//     set query (newQuery) {
-//         this.searchQuery = newQuery;
-//     }
+    set query (newQuery) {
+        this.searchQuery = newQuery;
+    }
 
-// }
+}
