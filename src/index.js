4c4b68dcb646bfc73eb7dff8e806f4c5afc0924e
Надія Ткач <nadiya77723@gmail.com>
2022-09-25 15:40:30 +0300

    ..

--- src/index.js
+++ src/index.js
@@ -1,86 +1,102 @@
-
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
-import  NewsApiService  from './news-service';
+import NewsApiService from './news-service';
 // import renderCard  from './renderCard';
 
 const refs = {
-    formEl: document.querySelector('.search-form'),
-    inputEl: document.querySelector('.search-form__input'),
-    buttonEl: document.querySelector('.search-form__button'),
-    galleryEl: document.querySelector('.gallery'),
-    buttonLoad: document.querySelector('.load-more'),
-}
+  formEl: document.querySelector('.search-form'),
+  inputEl: document.querySelector('.search-form__input'),
+  buttonEl: document.querySelector('.search-form__button'),
+  galleryEl: document.querySelector('.gallery'),
+  buttonLoad: document.querySelector('.load-more'),
+};
 
 const newsApiService = new NewsApiService();
 
 refs.formEl.addEventListener('submit', onSearch);
 refs.buttonLoad.addEventListener('click', onLoadMore);
 
+refs.buttonLoad.disabled = false;
 
 function onSearch(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    newsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
-    
-    if(newsApiService.query === '') {
-        Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-        refs.galleryEl.innerHTML = '';
-        return
-    }
+  newsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
 
+
+  // якщо нічого не введено в інпут видавай повідомлення немає такого зображення
+  if (newsApiService.query === '') {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
     refs.galleryEl.innerHTML = '';
-        
-    newsApiService.fetchImage().then(response => console.log(response)).then(data => {
-        renderCard(data);
-       
-        if (page >= response.hits) {
-            Notify.warning("We're sorry, but you've reached the end of search results.");
-            return
-        }
-    }).catch()      
-} 
+    return;
+  }
+ 
+  // якщо введено слово рендери розмітку на екраан,очищай інпут і роби кнопку завантажити ще активною
+  newsApiService
+    .fetchImage()
+    .then(data => {
+      renderCard(data);
+      resetPage();
+      // refs.buttonLoad.disabled = false;
+      
+    })
+    .catch();
+  onLoadMore()
+ 
+}
 
-function renderCard(data) {
-    const card = data.map(({webformatURL,
-       largeImageURL,
+onLoadMore();
+
+function renderCard(img) {
+  refs.galleryEl.insertAdjacentHTML('beforeend', markupGallery(img));
+}
+
+function markupGallery(data) {
+  return data
+    .map(
+      ({
+        largeImageURL,
         tags,
+        webformatURL,
         likes,
         views,
         comments,
-        downloads,}) => { 
-             return`<div class="photo-card">
-            <a href="${largeImageURL}">
-            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-            </a>
-            <div class="info">
-              <p class="info-item">
-                <b>Likes</b>
-                ${likes}
-              </p>
-              <p class="info-item">
-                <b>Views</b>
-                ${views}
-              </p>
-              <p class="info-item">
-                <b>Comments</b>
-                ${comments}
-              </p>
-              <p class="info-item">
-                <b>Downloads</b>
-                ${downloads}
-              </p>
-            </div>
-          </div>`
-    }).join('');
-
-    refs.galleryEl.insertAdjacentHTML('beforeend', card);
-    console.log(refs.galleryEl);
+        downloads,
+      }) => {
+        return `
+<div class="thumb">
+    <a href="${largeImageURL}"
+            class="gallery__item" >
+    <div class="photo-card">
+            <img src="${webformatURL}" alt="${tags}" width="300" height="300" loading="lazy"
+            class="gallery__image"/>
+        <div class="info">
+            <p class="info-item">
+            <b>Likes </b>${likes}
+            </p>
+            <p class="info-item">
+            <b>Views </b>${views}
+            </p>
+            <p class="info-item">
+            <b>Comments </b>${comments}
+            </p>
+            <p class="info-item">
+            <b>Downloads </b>${downloads}
+            </p>
+         </div>
+    </div>
+    </a>
+</div>`;
+      }
+    )
+    .join('');
 }
 
-
 function onLoadMore() {
-    buttonLoad.disable();
+    // buttonLoad.disabled = true;
+    incrementPage();
    fetchImage(query)
    .then(response => {
     if(response.total === 0) {
@@ -90,21 +106,83 @@ function onLoadMore() {
     }
     if (r.hits.length === 0) {
        buttonLoad.hide();
-        Notify.failure(
+        Notiflix.Notify.failure(
           `We're sorry, but you've reached the end of search results`
         );
         return;
       }
       renderCard(data);
-      buttonLoad.enable()
+      buttonLoad.disabled = false;
    })
    .catch(error => console.log(error));
 
 }
 
 
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   
   
